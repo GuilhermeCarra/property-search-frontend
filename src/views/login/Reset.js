60dadc86b19ff75e99bcf4a64caf1ff19a137858
Guilherme Carra <gui_gc@hotmail.com>
2020-11-05 18:35:30 +0100

    Add content to reset password view

--- src/views/login/Reset.js
+++ src/views/login/Reset.js
@@ -1,7 +1,30 @@
 function Reset() {
     return (
       <>
-        <h1>Reset Password</h1>
+        <div className="container login-card">
+          <div className="row">
+
+            <div className="col m6 offset-m3 center  z-depth-3">
+              <div className="section">
+                <h2>Reset Password</h2>
+                <div className="col s12">
+                  <div className="input-field col s8 offset-s2">
+                    <input id="password" type="password"/>
+                    <label for="password">Password</label>
+                  </div>
+                </div>
+                <div className="col s12">
+                  <div className="input-field col s8 offset-s2">
+                    <input id="password_confirm" type="password"/>
+                    <label for="password_cofirm">Cofirm Password</label>
+                  </div>
+                </div>
+                <a className="waves-effect waves-light btn-large orange lighten-1">Sign Up</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </>
     );
   }
