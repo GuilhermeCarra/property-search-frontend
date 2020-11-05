408e17fb043c94b9526ac1d13c75948065e2ab24
Guilherme Carra <gui_gc@hotmail.com>
2020-11-05 18:34:36 +0100

    Add main router

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import Auth from './routes/Auth.js'
 function App() {
   return (
     <Router>
-      <h1>Hey welcome home!</h1>
-      <div className="links">
-        <Link to='/login/' className="link">Login</Link>
-        <Link to='/login/signup' className="link">Sign Up</Link>
-      </div>
       <Switch>
         <Route path="/login">
           <Auth />
