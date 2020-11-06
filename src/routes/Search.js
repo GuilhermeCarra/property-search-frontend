9f272c83f34009ae8c2cd0bb2cae2efef2deef5f
Guilherme Carra <gui_gc@hotmail.com>
2020-11-06 09:14:21 +0100

    Add search route

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,13 +1,17 @@
 import { Switch, Route } from 'react-router-dom';
 import Main from '../views/search/Main.js';
+import Header from '../components/Header.js';
 
 function Search() {
   return (
+    <>
+    <Header />
     <Switch>
-      <Route exact path="/search">
+      <Route exact path="/">
         <Main />
       </Route>
     </Switch>
+    </>
   );
 }
 
