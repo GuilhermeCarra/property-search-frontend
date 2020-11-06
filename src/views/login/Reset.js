35742050f4dfcb0607fdb0374d67fa2d500826fd
Guilherme Carra <gui_gc@hotmail.com>
2020-11-06 07:54:48 +0100

    Change style on reset pass

--- src/views/login/Reset.js
+++ src/views/login/Reset.js
@@ -1,19 +1,19 @@
 function Reset() {
     return (
-      <>
-        <div className="container login-card">
+      <div className="orange page-container">
+        <div className="container">
           <div className="row">
 
-            <div className="col m6 offset-m3 center  z-depth-3">
-              <div className="section">
+            <div className="col m6 offset-m3 center z-depth-3 white center-card valign-wrapper">
+              <div className="section col s12 m12">
                 <h2>Reset Password</h2>
-                <div className="col s12">
+                <div className="col s12 m12">
                   <div className="input-field col s8 offset-s2">
                     <input id="password" type="password"/>
                     <label for="password">Password</label>
                   </div>
                 </div>
-                <div className="col s12">
+                <div className="col s12 m12">
                   <div className="input-field col s8 offset-s2">
                     <input id="password_confirm" type="password"/>
                     <label for="password_cofirm">Cofirm Password</label>
@@ -25,7 +25,7 @@ function Reset() {
 
           </div>
         </div>
-      </>
+      </div>
     );
   }
 
