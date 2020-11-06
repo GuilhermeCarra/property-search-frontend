2d2355ad4e177b1518b0fbfa1b25a71294bea4f3
Guilherme Carra <gui_gc@hotmail.com>
2020-11-06 09:14:32 +0100

    Add main view to search

---
+++ src/views/search/Main.js
@@ -0,0 +1,22 @@
+function Main() {
+    return (
+      <div className="orange lighten-2 searchpage-container">
+        <div className="row">
+
+          <div className="col s12 center white-text h-100 bg-text">
+            <div className="section">
+              <h4>Let's find a home that is perfect for you.</h4>
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="local" type="text" class="validate"/>
+                  <label for="local">Where do you want to live?</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default Main;
