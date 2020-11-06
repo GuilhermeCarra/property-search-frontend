6ccd140858dd529dd9850a195303a70583a76b9c
Guilherme Carra <gui_gc@hotmail.com>
2020-11-06 08:18:37 +0100

    Change style on reset password view

--- src/views/login/Reset.js
+++ src/views/login/Reset.js
@@ -4,16 +4,16 @@ function Reset() {
         <div className="container">
           <div className="row">
 
-            <div className="col m6 offset-m3 center z-depth-3 white center-card valign-wrapper">
+            <div className="col s12 m10 offset-m1 center z-depth-3 white center-card valign-wrapper">
               <div className="section col s12 m12">
                 <h2>Reset Password</h2>
-                <div className="col s12 m12">
+                <div className="col s12">
                   <div className="input-field col s8 offset-s2">
                     <input id="password" type="password"/>
                     <label for="password">Password</label>
                   </div>
                 </div>
-                <div className="col s12 m12">
+                <div className="col s12">
                   <div className="input-field col s8 offset-s2">
                     <input id="password_confirm" type="password"/>
                     <label for="password_cofirm">Cofirm Password</label>
