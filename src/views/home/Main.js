652efe34c064c452b57422a2f0b6e74386602acb
Guilherme Carra <gui_gc@hotmail.com>
2020-11-09 19:30:19 +0100

    Change main view

--- src/views/home/Main.js
+++ src/views/home/Main.js
@@ -1,22 +1,32 @@
-function Main() {
-    return (
-      <div className="orange lighten-2 searchpage-container">
-        <div className="row">
+import { useHistory } from "react-router-dom";
 
-          <div className="col s12 center white-text h-100 bg-text">
-            <div className="section">
-              <h4>Let's find a home that is perfect for you.</h4>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="local" type="text" class="validate"/>
-                  <label for="local">Where do you want to live?</label>
-                </div>
+function Main() {
+  let history = useHistory();
+
+  function handleSubmit(e) {
+    if(e.key === 'Enter'){
+      history.push(`/search?location=${e.target.value}`)
+    }
+  }
+
+  return (
+    <div className="orange lighten-2 searchpage-container">
+      <div className="row">
+
+        <div className="col s12 center white-text h-100 bg-text">
+          <div className="section">
+            <h4>Let's find a home that is perfect for you.</h4>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="local" type="text" class="validate" onKeyPress={(e) => handleSubmit(e)}/>
+                <label for="local">Where do you want to live?</label>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Main;
