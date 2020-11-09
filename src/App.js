0af7426d5a4f0e54d17d8a6d17425a3804cbf883
Guilherme Carra <gui_gc@hotmail.com>
2020-11-09 08:47:57 +0100

    Set up Redux on project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from "./store/store";
 import './App.scss';
 import Auth from './routes/Auth.js'
 import Search from './routes/Search.js'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Auth />
-        </Route>
-        <Route path="/">
-          <Search />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Auth />
+          </Route>
+          <Route path="/">
+            <Search />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
