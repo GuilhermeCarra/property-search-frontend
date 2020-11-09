d4ae6c538c002d6b2097e76fc867b62419d5e321
Guilherme Carra <gui_gc@hotmail.com>
2020-11-09 19:28:49 +0100

    Create function to register request

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,23 @@
 export const apiDomain = "http://localhost:3000/";
 
 // Auth URIs
-export const loginRequest = {
-    url: apiDomain + "login",
-    config: { "method": 'POST' }
+export const loginRequest = function({password, email}) {
+    return {
+        method: 'post',
+        url: apiDomain + "login",
+        data: {
+            password: password,
+            email: email
+        }
+    }
+};
+
+export const registerRequest = function({password, email}) {
+    return {
+        method: 'post',
+        url: apiDomain + "register",
+        data: {
+            
+        }
+    }
 };
