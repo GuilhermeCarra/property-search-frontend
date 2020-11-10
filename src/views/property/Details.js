a34daab09dd2cbcfdd7f2a7e03ba89ca7248d6e5
Guilherme Carra <gui_gc@hotmail.com>
2020-11-10 15:55:10 +0100

    Create property detail page

---
+++ src/views/property/Details.js
@@ -0,0 +1,28 @@
+function Details() {
+    return(
+    <>
+        <div className="">
+            <p className="col">⯇ Back to search results</p>
+        </div>
+        <div className="">
+            <div className="row">
+                <div className="col s12 m6 lighten-3">
+                    <img className="col s12" src="https://materializecss.com/images/office.jpg"/>
+                    <div className="col s12 center">Photos</div>
+                </div>
+                <div className="col s12 m6">
+                    <h6>4 bed semi-detached house for sale</h6>
+                    <p>Barcelona</p>
+                    <h5>£400,000</h5>
+                    <p>3 Bedrooms</p>
+                    <p>2 Bathrooms</p>
+                    <button className="col s9 m9 waves-effect waves-light btn-large orange lighten-1">Contact</button>
+                    <button className="col s2 m2 offset-s1 waves-effect waves-light btn-large orange lighten-1 red">❤</button>
+                </div>
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default Details;
