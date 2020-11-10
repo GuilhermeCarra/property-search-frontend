d5303e2e475bb5b990f3cfd12d36c20c2e4339eb
Guilherme Carra <gui_gc@hotmail.com>
2020-11-10 15:54:13 +0100

    Change materialize grid style

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Properties() {
     return(
     <div className="section">
@@ -8,7 +10,7 @@ function Properties() {
                 </div>
                 <div className="card-content">
                     <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">...</i></span>
-                    <p><a href="#">This is a link</a></p>
+                    <p><Link to="property/id">This is a link</Link></p>
                 </div>
                 <div className="card-reveal">
                     <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">x</i></span>
@@ -16,14 +18,14 @@ function Properties() {
                 </div>
             </div>
         </div>
-        <div className="col m4 offset-m1">
+        <div className="col m4 offset-m2">
             <div className="card">
                 <div className="card-image waves-effect waves-block waves-light">
                     <img className="activator" src="https://materializecss.com/images/office.jpg"/>
                 </div>
                 <div className="card-content">
                     <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">...</i></span>
-                    <p><a href="#">This is a link</a></p>
+                    <p><Link to="property/id">This is a link</Link></p>
                 </div>
                 <div className="card-reveal">
                     <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">x</i></span>
